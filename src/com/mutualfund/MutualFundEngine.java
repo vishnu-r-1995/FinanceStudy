069b4043683d57f7324f95d2fd2a57e82a603269
vishnu-r-1995 <vishnuvjd@gmail.com>
2022-12-24 18:14:59 +0530

    Display Yearly SIP Principal on Maturity

--- src/com/mutualfund/MutualFundEngine.java
+++ src/com/mutualfund/MutualFundEngine.java
@@ -6,6 +6,7 @@ public class MutualFundEngine {
 
 	public static void main(String[] args) {
 		displaySIPPrincipalOnMaturity(12, 24, new BigDecimal(2000.0));
+		displayYearlySIPPrincipalOnMaturity(12.0, 22, new BigDecimal(100000.0));
 	}
 
 	private static void displaySIPPrincipalOnMaturity(double interest, int numberOfInstallments, BigDecimal investmentPerMonth) 
@@ -28,4 +29,24 @@ public class MutualFundEngine {
 		return new BigDecimal(interest).multiply(principal).divide(denominator);
 	}
 
+	private static void displayYearlySIPPrincipalOnMaturity(double interest, int numberOfYears, BigDecimal investmentPerYear) 
+	{
+		BigDecimal principal = investmentPerYear;
+		for (int i = 1; i < numberOfYears; i++) {
+			principal = principal.add(getYearlyInterestAsBigDecimal(interest, principal));
+			principal = principal.add(investmentPerYear);
+		}
+		//Add interest for the last installment as per calculation given in "https://cleartax.in/s/sip-calculator"
+		principal = principal.add(getYearlyInterestAsBigDecimal(interest, principal));
+		
+		System.out.println("Principal at the end of " + numberOfYears + " years of Rs."
+				+ investmentPerYear + " per year at " + interest + "% interest = " + Math.round(principal.doubleValue()));
+		
+	}
+
+	private static BigDecimal getYearlyInterestAsBigDecimal(double interest, BigDecimal principal) 
+	{
+		BigDecimal denominator = new BigDecimal(100);
+		return new BigDecimal(interest).multiply(principal).divide(denominator);
+	}
 }
