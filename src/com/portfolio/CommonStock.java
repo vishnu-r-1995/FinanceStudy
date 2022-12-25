9ff52b37164be4287c04ec20aab544e992bba356
vishnu-r-1995 <vishnuvjd@gmail.com>
2022-12-25 16:20:07 +0530

    Add initial paramters of Common Stock pojo class

--- src/com/portfolio/CommonStock.java
+++ src/com/portfolio/CommonStock.java
@@ -1,5 +1,35 @@
 package com.portfolio;
 
+import java.math.BigDecimal;
+
 public class CommonStock {
 
+	private String symbol;
+	private String name;
+	private BigDecimal previousClosingPrice = BigDecimal.ZERO;
+	private BigDecimal currentPrice = BigDecimal.ZERO;
+	private double workingCapitalRatio = 0d;
+	private BigDecimal upsideTargetPrice = BigDecimal.ZERO;
+	private BigDecimal downsideTargetPrice = BigDecimal.ZERO;
+	private double riskRewardRatio = 0d;
+	
+	public CommonStock(String symbol, String name, BigDecimal previousClosingPrice, BigDecimal currentPrice,
+			double workingCapitalRatio, BigDecimal upsideTargetPrice, BigDecimal downsideTargetPrice) {
+		super();
+		this.symbol = symbol;
+		this.name = name;
+		this.previousClosingPrice = previousClosingPrice;
+		this.currentPrice = currentPrice;
+		this.workingCapitalRatio = workingCapitalRatio;
+		this.upsideTargetPrice = upsideTargetPrice;
+		this.downsideTargetPrice = downsideTargetPrice;
+		this.riskRewardRatio = getRiskRewardRatio(currentPrice, upsideTargetPrice, downsideTargetPrice);
+	}
+
+	public double getRiskRewardRatio(BigDecimal current, BigDecimal upside, BigDecimal downside) 
+	{
+		BigDecimal numerator = upside.subtract(current);
+		BigDecimal denominator = current.subtract(downside);
+		return numerator.divide(denominator).doubleValue();
+	}
 }
