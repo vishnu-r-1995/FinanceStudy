318625c266db0e83eab219b5df882ab6c48c3260
vishnu-r-1995 <vishnuvjd@gmail.com>
2022-12-25 16:41:36 +0530

    Change parameterized constructor
    From the parameters, downside target price is replaced with risk reward ratio. Because, the risk reward ratio is specified by the user while the downside target price needs to be calculated.

--- src/com/portfolio/CommonStock.java
+++ src/com/portfolio/CommonStock.java
@@ -14,7 +14,7 @@ public class CommonStock {
 	private double riskRewardRatio = 0d;
 	
 	public CommonStock(String symbol, String name, BigDecimal previousClosingPrice, BigDecimal currentPrice,
-			double workingCapitalRatio, BigDecimal upsideTargetPrice, BigDecimal downsideTargetPrice) {
+			double workingCapitalRatio, BigDecimal upsideTargetPrice, double riskRewardRatio) {
 		super();
 		this.symbol = symbol;
 		this.name = name;
@@ -22,14 +22,29 @@ public class CommonStock {
 		this.currentPrice = currentPrice;
 		this.workingCapitalRatio = workingCapitalRatio;
 		this.upsideTargetPrice = upsideTargetPrice;
-		this.downsideTargetPrice = downsideTargetPrice;
-		this.riskRewardRatio = getRiskRewardRatio(currentPrice, upsideTargetPrice, downsideTargetPrice);
+		this.riskRewardRatio = riskRewardRatio;
+		this.downsideTargetPrice = getDownsideTargetPrice(currentPrice, upsideTargetPrice, riskRewardRatio);
 	}
 
 	public double getRiskRewardRatio(BigDecimal current, BigDecimal upside, BigDecimal downside) 
 	{
+		// (upside-current)/(current-downside) = risk-reward ratio
+		// Some investors won't commit their money to any investment that isn't at least
+		// 4:1, but 2:1 is considered the minimum by most
+		// "https://www.investopedia.com/articles/stocks/11/calculating-risk-reward.asp"
 		BigDecimal numerator = upside.subtract(current);
 		BigDecimal denominator = current.subtract(downside);
 		return numerator.divide(denominator).doubleValue();
+	}
+	
+	private BigDecimal getDownsideTargetPrice(BigDecimal current, BigDecimal upside, double riskReward) 
+	{
+		// (upside-current)/(current-downside) = risk-reward ratio
+		// Some investors won't commit their money to any investment that isn't at least
+		// 4:1, but 2:1 is considered the minimum by most
+		// "https://www.investopedia.com/articles/stocks/11/calculating-risk-reward.asp"
+		BigDecimal numerator = upside.subtract(current);
+		BigDecimal denominator = numerator.divide(new BigDecimal(riskReward));
+		return current.subtract(denominator);
 	}
 }
--- src/com/portfolio/CommonStockStorage.java
+++ src/com/portfolio/CommonStockStorage.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class CommonStockStorage {
 
 	public static List<CommonStock> commonstocks = Arrays.asList(
-			new CommonStock("goog", "google", new BigDecimal(89.23), new BigDecimal(89.23), 2.46, new BigDecimal(122.20), new BigDecimal(0)),
-			new CommonStock("aapl", "apple", new BigDecimal(131.86), new BigDecimal(131.86), 0.86, new BigDecimal(175), new BigDecimal(0))
+			new CommonStock("goog", "google", new BigDecimal(89.23), new BigDecimal(89.23), 2.46, new BigDecimal(122.20), 2),
+			new CommonStock("aapl", "apple", new BigDecimal(131.86), new BigDecimal(131.86), 0.86, new BigDecimal(175), 2)
 			);
 }
