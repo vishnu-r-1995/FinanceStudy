66207768458f8690d24621fd977f3da2fd8401f0
vishnu-r-1995 <vishnuvjd@gmail.com>
2022-12-25 16:30:45 +0530

    Add common stock storage class as database for common stocks

---
+++ src/com/portfolio/CommonStockStorage.java
@@ -0,0 +1,13 @@
+package com.portfolio;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+public class CommonStockStorage {
+
+	public static List<CommonStock> commonstocks = Arrays.asList(
+			new CommonStock("goog", "google", new BigDecimal(89.23), new BigDecimal(89.23), 2.46, new BigDecimal(122.20), new BigDecimal(0)),
+			new CommonStock("aapl", "apple", new BigDecimal(131.86), new BigDecimal(131.86), 0.86, new BigDecimal(175), new BigDecimal(0))
+			);
+}
