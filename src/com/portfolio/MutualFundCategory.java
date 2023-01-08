fd8fa48df701a1648047f0a167ccd89954b679fa
vishnu-r-1995 <vishnuvjd@gmail.com>
2023-01-08 13:39:47 +0530

    Add comment to point to source of mf categories

--- src/com/portfolio/MutualFundCategory.java
+++ src/com/portfolio/MutualFundCategory.java
@@ -1,6 +1,8 @@
 package com.portfolio;
 
 public enum MutualFundCategory {
+	//Source : "https://www.amfiindia.com/investor-corner/knowledge-center/SEBI-categorization-of-mutual-fund-schemes.html"
+	
 	UNCATEGORIZED,
 	LARGECAPFUND,
 	MULTICAPFUND,
